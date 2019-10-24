01ed1d0811d3c4fb521243df457066f52ccfaecf
Administrator <asd19999@sina.com>
2019-10-24 20:02:29 +0800

    Initial commit

---
+++ android/app/src/main/java/com/julong/julong_machine_test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.julong.julong_machine_test;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
